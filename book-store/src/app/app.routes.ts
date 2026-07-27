12c792c67ef804d2a2232849c84b55e72df00443
Shimaa12Ahmed <168415906+Shimaa12Ahmed@users.noreply.github.com>
2026-07-27 18:22:58 +0300

    solve conflict

--- book-store/src/app/app.routes.ts
+++ book-store/src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import {Login} from '../pages/auth/login/login';
+import { Register } from '../pages/auth/register/register';
+import { Home } from '../pages/home/home';
+import { Admin } from '../pages/admin/admin';
+import { AdminLayout } from '../Layouts/admin-layout/admin-layout';
+import { CustomerLayout } from '../Layouts/customer-layout/customer-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",redirectTo:"home",pathMatch:"full"},
+    {path:"home",component:Home},
+    {path:"login",component:Login},
+    {path:"sign-up",component:Register},
+    {path:"admin",component:Admin},
+
+
+  {
+    path: '',
+    component: CustomerLayout,
+    children: [
+      // { path: '', component: Home },
+      // { path: 'books', component: Books },
+      // { path: 'books/:id', component: BookDetails },
+      // { path: 'login', component: Login },
+      // { path: 'register', component: Register },
+      // { path: 'wishlist', component: Wishlist },
+      // { path: 'cart', component: Cart },
+      // { path: 'checkout', component: Checkout },
+    ]
+  },
+  {
+    path: 'admin',
+    component: AdminLayout,
+    // canActivate: [adminGuard],
+    children: [
+      // { path: 'dashboard', component: Dashboard },
+      // { path: 'add-book', component: AddBook },
+      // { path: 'manage-books', component: ManageBooks },
+      // { path: 'sales-analytics', component: SalesAnalytics },
+      // { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ]
+  },
+  // { path: '**', component: NotFound }
+];
