3fadbac230bca07bb29e6738c609eff49254032a
Shimaa12Ahmed <168415906+Shimaa12Ahmed@users.noreply.github.com>
2026-07-27 19:38:39 +0300

    Update app.ts

--- book-store/src/app/app.ts
+++ book-store/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Nav} from '../shared/components/nav/nav'
+import { Login } from '../pages/auth/login/login';
+import { Register } from '../pages/auth/register/register';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Nav],
+  imports: [RouterOutlet,Nav,Login,Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
